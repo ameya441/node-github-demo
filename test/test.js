333a17fe92b42c9466e6145231cd65ff77b248f1
ameya441 <barwe.ameya@gmail.com>
2021-10-19 21:45:26 +0800

    modify test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-  it("respond with Hello World", (done) => {
+  it("respond with Hello World - GITHUB ACTIONS", (done) => {
     request(app).get("/").expect("Hello World", done);
   })
 });
