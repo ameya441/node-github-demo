a6d2e4075663c3e38c7628d18c6b4fc35faa5092
ameya441 <barwe.ameya@gmail.com>
2021-10-19 22:18:37 +0800

    your comment

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("../index");
-
-describe("GET /", () => {
-  it("respond with Hello World - GITHUB ACTIONS", (done) => {
-    request(app).get("/").expect("Hello World", done);
-  })
-});
