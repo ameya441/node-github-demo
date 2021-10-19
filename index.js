d9d04565306f743efc2e99a623a9838b08048c18
ameya441 <barwe.ameya@gmail.com>
2021-10-19 22:08:57 +0800

    modify index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello World, GITHUB ACTIONS");
+ return res.send("Hello World, GITHUB ACTIONS - AMEYA");
 });
 
 // listen to port 7000 by default
